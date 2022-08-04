6d00afec5186fc9845b00e0f1947a97eed7225f7
eduardo-orn <eduardo.lizarde@itsn.edu.mx>
2022-08-04 00:43:09 -0500

    modificacion

--- source/repos/University.API/University/Controllers/HomeController.cs
+++ source/repos/University.API/University/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace University.Controllers
 {
+    //este es el from controller
     public class HomeController : Controller
     {
         public ActionResult Index()
