c50f7fa96d2ffffb682e77b34baf760613d086f4
eduardo-orn <eduardo.lizarde@itsn.edu.mx>
2022-08-04 00:07:49 -0500

    back

---
+++ source/repos/University.API/University.BL/DTOs/CourseDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace University.BL.DTOs
+{
+    public class CourseDTO
+    {
+        [Required(ErrorMessage = "The field course ID is required")]
+        public int CourseID { get; set; }
+
+        [Required(ErrorMessage = "The field title required")]
+        [StringLength(50)]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "The field credits is required")]
+        public int Credits { get; set; }
+    }
+}
---
+++ source/repos/University.API/University/App_Start/WebApiConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using University.Controllers;
+
+namespace University
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuración y servicios de Web API
+            var enableCorseAttribute = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(enableCorseAttribute);
+
+            // Rutas de Web API
+            config.MapHttpAttributeRoutes();
+
+            config.MessageHandlers.Add(new TokenValidationHandler());
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
